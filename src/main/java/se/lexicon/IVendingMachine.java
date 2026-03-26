9c4c18483a25e5aad8c98beafb964b181f033773
Shanmuga Saravanan <sunsarav16@gmail.com>
2026-03-26 10:16:19 +0100

    Added subclasses and implemented the methods in Interface

--- src/main/java/se/lexicon/IVendingMachine.java
+++ src/main/java/se/lexicon/IVendingMachine.java
@@ -1,4 +1,13 @@
 package se.lexicon;
 
+import java.util.List;
+
 public interface IVendingMachine {
+
+    void insertCoin(int coin);
+    int getBalance();
+    void purchaseProduct(int productId);
+    int returnChange();
+    List<Product> getProducts();
+
 }
--- src/main/java/se/lexicon/Product.java
+++ src/main/java/se/lexicon/Product.java
@@ -1,4 +1,34 @@
 package se.lexicon;
 
-public class Product {
+public abstract class Product {
+    private int id;
+    private String name;
+    private int price;
+    private int quantity;
+
+    public Product(int id, String name, int price, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public abstract String getDescription();
+
 }
